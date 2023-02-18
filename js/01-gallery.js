503c312d2fac0ffa03e84fe6004f1793eafac2d9
Taras <112703291+Shadow-SML@users.noreply.github.com>
2023-02-18 16:57:12 +0100

    correct task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,59 +1,57 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const containerGallery = document.querySelector(".gallery");
-const itemsMarkup = createGalleryItems(galleryItems);
 
-containerGallery.insertAdjacentHTML("beforeend", itemsMarkup);
-containerGallery.addEventListener("click", onContainerGalleryClick);
+if (!galleryItems || !containerGallery) {
+  console.error("Failed to initialize the gallery.");
+}
 
-function createGalleryItems(galleryItems) {
+containerGallery.addEventListener(
+  "click",
+  function handleGalleryItemClick(event) {
+    event.preventDefault();
+    const target = event.target;
+    const isItemLink = target.classList.contains("gallery__image");
+    if (!isItemLink) {
+      return;
+    }
+    const originalSrc = target.dataset.source;
+    const lightboxInstance = basicLightbox.create(
+      `<img src="${originalSrc}" width="1240" height="800">`,
+      {
+        onShow: () => window.addEventListener("keydown", handleModalClose),
+        onClose: () => window.removeEventListener("keydown", handleModalClose),
+      }
+    );
+    lightboxInstance.show();
+
+    function handleModalClose(event) {
+      const ESC_KEY = "Escape";
+      if (event.code === ESC_KEY) {
+        lightboxInstance.close();
+      }
+    }
+  }
+);
+
+function renderGallery(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-  </div>
-    `;
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
     })
     .join("");
 }
 
-function onContainerGalleryClick(event) {
-  event.preventDefault();
-  const isItemLink = event.target.classList.contains("gallery__image");
-
-  if (!isItemLink) {
-    return;
-  }
-
-  const onCloseModal = (event) => {
-    const ESC_KEY = "Escape";
-
-    if (event.code === ESC_KEY) {
-      instance.close();
-    }
-  };
-
-  const instance = basicLightbox.create(
-    `
-      <img src="${event.target.dataset.source}" width="1240" height="800">`,
-    {
-      onShow: (instance) => {
-        window.addEventListener("keydown", onCloseModal);
-      },
-
-      onClose: (instance) => {
-        window.removeEventListener("keydown", onCloseModal);
-      },
-    }
-  );
-  instance.show();
-}
+const itemsMarkup = renderGallery(galleryItems);
+containerGallery.insertAdjacentHTML("beforeend", itemsMarkup);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const containerGallery = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+containerGallery.insertAdjacentHTML("beforeend", itemsMarkup);
+
+initLightbox();
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+              width="400"
+              height="300"
+              title="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+function initLightbox() {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "title",
+    scrollZoom: false,
+  });
+}
